af0b7e877fa403ec354c3690c241bc2891b66b1f
Kalpa D <KALPAFERNANDO1998@gmail.com>
2020-05-17 11:46:49 +0530

    server side validation completed and added a new field to user on signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,33 +9,28 @@ exports.login = (req, res, next) => {
     const password = req.body.password
 }
 
-
 exports.signup = (req, res, next) => {
+    errorHandler(req)
     const email = req.body.email;
     const password = req.body.password
-    User.findOne({email: email}).then(
-        result => {
-            if (result) {
-                return res.status(403).json({message: 'User already exist'})
-            } else {
-                bcrypt.hash(password, 10).then(
-                    hashedPassword => {
-                        const user = new User({
-                            email: email,
-                            password: hashedPassword
-                        });
-                        user.save()
-                            .then(result => {
-                                res.status(201).json({message: "user created successfully", user: result})
-                            })
-                            .catch(e => {
-                                res.status(500).json({
-                                    message: e + 'Server Error : failed to hash password try again later'
-                                });
-                            })
-                    })
-                    .catch(err => console.log(err))
-            }
-        }
-    )
+    const name = req.body.name // needs to e added on front end
+    bcrypt.hash(password, 10).then(
+        hashedPassword => {
+            const user = new User({
+                email: email,
+                name: name,
+                password: hashedPassword
+            });
+            user.save()
+                .then(result => {
+                    res.status(201).json({message: "user created successfully", user: result})
+                })
+                .catch(e => {
+                    res.status(500).json({
+                        message: 'Server Error : failed to hash password try again later\n\n' + e
+                    });
+                })
+        })
+        .catch(err => console.log(err))
+
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth')
+const {body} = require('express-validator');
+const User = require('../models/user')
+
 
 /* GET home page. */
 router.post('/login', authController.login);
 
 
-
-
 router.post(
     '/signup',
-
+    [
+        body('email')
+            .trim()
+            .isEmail()
+            .withMessage('Please enter a valid email')
+            .custom((value, {req}) => {
+                return User.findOne({email: value}).then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject('Email already exists')
+                    }
+                })
+            }).normalizeEmail(),
+        body('password')
+            .trim()
+            .isLength({min: 5}),
+        body('name')
+            .trim()
+            .isLength({min: 3}),
+    ],
     authController.signup);
 
 module.exports = router;
