233a3b87d1d79c23afd570bfab21fb7b0a19170b
Kalpa <kalpafernando1998@gmail.com>
2020-05-19 12:41:08 +0530

    added middleware to authenticate loggedIn user

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,12 +1,30 @@
 const jwt = require('jsonwebtoken');
-const errorThrower = require('../services/errorThrower')
+const throwError = require('../services/throwError')
 
 const authHandler = (req, res, next) => {
 
     const authHeader = req.get('Authorization')
 
-    if(!authHeader){
+    if (!authHeader) {
+        throwError('Not Authenticated', 401)
     }
+
+    const token = authHeader.split(' ')[1];
+    let decodedToken
+
+    try {
+        decodedToken = jwt.verify(token, process.env.SECRET)
+    } catch (e) {
+        e.statusCode = 500;
+        throw e
+    }
+
+    if (!decodedToken) {
+        throwError('Not authenticated', 401)
+    }
+
+    req.userId = decodedToken.userId
+    next()
 }
 
 
