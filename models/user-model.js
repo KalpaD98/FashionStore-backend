22da304fd367df5001d42b76bea50a2544dffe39
ChethiyaNishanath <chethiyanishanath@gmail.com>
2020-05-22 21:10:12 +0530

    user model added

---
+++ models/user-model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum:['admin','store-manager','user'],
+        default: 'user'
+    }
+})
+
+
+userSchema.plugin(uniqueValidator)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
