433cbea8078eb8609f573a4dc30e2b5fe1e4868c
Kalpa <kalpafernando1998@gmail.com>
2020-05-24 19:46:33 +0530

    added User reference to item-model

--- models/item-model.js
+++ models/item-model.js
@@ -1,24 +1,24 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const itemSchema=new mongoose.Schema({
+const itemSchema = new mongoose.Schema({
 
     itemName: {
         type: String,
-        required: [true,'An item must have a name']
+        required: [true, 'An item must have a name']
     },
     category: {
         type: String,
-        enum:['Ladies','Gents','Unisex','Teenage-Boys','Teenage-Grls','Boys','Girls','Babies','Other'],
-        required: [true,'An item must have a category']
+        enum: ['Ladies', 'Gents', 'Unisex', 'Teenage-Boys', 'Teenage-Grls', 'Boys', 'Girls', 'Babies', 'Other'],
+        required: [true, 'An item must have a category']
     },
-    itemType:{
+    itemType: {
         type: String,
-        enum: ['Trouser','Shirt','T-shirt'],
+        enum: ['Trouser', 'Shirt', 'T-shirt'],
         required: true
     },
     price: {
         type: Number,
-        required: [true,'An item must have a price'],
+        required: [true, 'An item must have a price'],
         default: 0.0
     },
     description: {
@@ -27,28 +27,34 @@ const itemSchema=new mongoose.Schema({
     },
     itemPhotos: {
         type: String,
-        default:"default-img.jpg"
+        default: "default-img.jpg"
     },
     country: {
         type: String
     },
-    itemQuantity:{
+    itemQuantity: {
         type: Number,
         required: true
     },
-    ratingAverage:{
+    ratingAverage: {
         type: Number,
-        min: [0,'A rate cannot be lesser than 0'],
-        max: [5,'A rate cannot be greater than 5'],
+        min: [0, 'A rate cannot be lesser than 0'],
+        max: [5, 'A rate cannot be greater than 5'],
         default: 4.0
     },
-    ratingQuantity:{
+    ratingQuantity: {
         type: Number,
         default: 0
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
     }
+
 });
 
 
-const Item=mongoose.model('Item',itemSchema);
+const Item = mongoose.model('Item', itemSchema);
 
 module.exports = Item;
